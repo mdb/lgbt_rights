c45355a77292c95dd6afa951afa320541f6705ec
Casey Thomas <cpthomas@gmail.com>
2012-04-16 12:25:47 -0400

    .gitignore is now working

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  //app.set('view options', { layout: false });
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.static(__dirname + '/public'));
