1b4bdfc9eed17e02c575f57a459dcb9f7ea09de3
Alex Yule <alexy@codeforamerica.org>
2012-04-28 14:10:36 -0400

    rough db.js

--- routes/db.js
+++ routes/db.js
@@ -3,3 +3,29 @@ if (process.env.REDISTOGO_URL) {
 } else {
   var redis = require('redis').createClient();
 }
+
+exports.rights = function(state, county, city, fn){
+	var result = {};
+	result['state'] = state(state);
+	result['county'] = county(state, county);
+	result['city'] = city(state, county, city);
+ 	fn(null, result);  
+}
+
+// takes in a json file and loads it into redis
+exports.load = function(json, fn){
+	
+}
+
+function state(state){
+	return redis.get(state);
+}
+
+function county(state, county){
+	return redis.get(state + ':' + county);
+}
+
+function city(state, county, city){
+	return redis.get(state + ':' + county + ':' +city);
+}
+
