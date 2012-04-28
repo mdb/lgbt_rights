ad58fb59e27b0eac1edf212c5c66b29df87dea2c
Mike Ball <mikedball@gmail.com>
2012-04-28 13:50:07 -0400

    Fixed to 2 space indenting.

--- public/javascripts/pgn.core.js
+++ public/javascripts/pgn.core.js
@@ -1,24 +1,24 @@
 if (typeof PGN === 'undefined' || !PGN) {
-    var PGN = {};
+  var PGN = {};
 }
 
 if (typeof PGN === 'undefined' || !PGN) {
-    var PGN = {};
+  var PGN = {};
 }
 
 PGN.core = (function ($) {
-    var _self;
+  var _self;
 
-    _self = {
-        injectRights: function () {
-          //$('body').append(_.template(_self.buildRightsTemplate()));
-        },
+  _self = {
+      injectRights: function () {
+        //$('body').append(_.template(_self.buildRightsTemplate()));
+      },
 
-        // TODO
-        buildRightsTemplate: function () {
-          return '<li>Foo</li>'; 
-        }
-    };
+      // TODO
+      buildRightsTemplate: function () {
+        return '<li>Foo</li>'; 
+      }
+  };
 
-    return _self;
+  return _self;
 })(jQuery);
