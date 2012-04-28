959f75c28744d1ac7e6be4362a89556a1fa4359f
Mike Ball <mikedball@gmail.com>
2012-04-28 11:51:43 -0400

    Added stylus app configuration.

--- app.js
+++ app.js
@@ -15,10 +15,14 @@ app.configure(function(){
   //app.set('view options', { layout: false });
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(require("stylus").middleware({
+    src: __dirname + "/public",
+    compress: true
+  }));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
