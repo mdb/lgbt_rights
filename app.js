4d6010632c936e4360d2631ca87c163a1deb839e
Gabriel Farrell <g@grrawr.com>
2012-04-28 13:15:29 -0400

    Start at process.env.PORT if supplied

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
   , routes = require('./routes')
-  , http = require('http');
-
-var app = express();
+  , http = require('http')
+  , app = express()
+  , port = process.env.PORT || 3000
+;
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -18,8 +18,8 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(require("stylus").middleware({
-    src: __dirname + "/public",
+  app.use(require('stylus').middleware({
+    src: __dirname + '/public',
     compress: true
   }));
   app.use(express.static(__dirname + '/public'));
@@ -31,6 +31,6 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(port);
 
-console.log("Express server listening on port 3000");
+console.log('Express server listening on port ' + port);
